b85fc6b33119aad01ba44481cea8cd78a69a1b99
ajawid158 <ajawid@auaf.edu.af>
2023-10-10 13:23:40 +0430

    CIIntro1010

--- CIIntro.R
+++ CIIntro.R
@@ -13,4 +13,15 @@ quantile(z, 0.025)
 #for 99% CI
 quantile(z, 0.005)
 
+##CI for population proportion 
+#mean=p, sd=sqrt(pq/n)
+
+qnorm(0.025)
+
+#95%CI=[phat-1.96*SQRT(p1/n), phat+1.96*SQRT(p1/n)]
+
+
+qnorm(0.05)
+
+#95%CI=[phat-1.64*SQRT(p1/n), phat+1.64*SQRT(p1/n)]
 
