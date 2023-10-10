fe6d9a2dc454ef0e79cad5b747a2be3f7b6e26dd
ajawid158 <ajawid@auaf.edu.af>
2023-10-10 13:16:23 +0430

    clt1010

--- clt.R
+++ clt.R
@@ -29,3 +29,24 @@ abline(v=mean(x$MT), col='red', lty=3, lwd=3)
 #par(mfrow=c(3,2))
 #
 #dev.off()
+
+#CLT for Sample proportion
+
+nSim=10000    #number of repetition
+
+n=100    #sample size 
+m1=c()
+
+for (i in 1:nSim) {
+  s=sample(x$Grade, n, replace = T)
+  m1[i]=mean(s)
+  
+}
+length(m1)
+
+
+hist(m1)
+abline(v=mean(x$Grade), col='red', lty=3, lwd=3)
+
+prop.table(table(x$Grade))
+
